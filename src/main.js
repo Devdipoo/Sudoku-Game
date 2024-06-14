d712c5e605cef336397233be3e9e23ed43de4c90
Devdipoo <emiolaajileye@gmail.com>
2024-06-14 09:43:57 -0700

    Save Game

--- src/main.js
+++ src/main.js
@@ -51,8 +51,8 @@ function setGame() {
     // Board 9x9
     for (let r = 0; r < 9; r++) {
         for (let c = 0; c < 9; c++) {
-            // let tile = document.createElement("div");
-            // tile.id = r.toString() + "-" + c.toString();
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
             if (board[r][c] != "-") {
                 tile.innerText = board[r][c];
                 tile.classList.add("tile-start");
