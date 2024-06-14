ec00f6b39608d9452de603bef103185a157ce311
Devdipoo <emiolaajileye@gmail.com>
2024-06-14 09:42:37 -0700

    Undo/Redo Functionality

--- src/main.js
+++ src/main.js
@@ -50,7 +50,7 @@ function setGame() {
 
     // Board 9x9
     for (let r = 0; r < 9; r++) {
-        for (let c = 0; c < 9; c++) {
+        // for (let c = 0; c < 9; c++) {
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
             if (board[r][c] != "-") {
