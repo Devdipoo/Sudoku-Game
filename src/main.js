7669f9b25fb58298d46ef894bcb00b0fa7b08ac3
Devdipoo <emiolaajileye@gmail.com>
2024-06-14 09:44:32 -0700

    Bug Fixes and Optimizations

--- src/main.js
+++ src/main.js
@@ -53,10 +53,10 @@ function setGame() {
         for (let c = 0; c < 9; c++) {
             let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
-            if (board[r][c] != "-") {
-                tile.innerText = board[r][c];
-                tile.classList.add("tile-start");
-            }
+            // if (board[r][c] != "-") {
+            //     tile.innerText = board[r][c];
+            //     tile.classList.add("tile-start");
+            // }
             if (r == 2 || r == 5) {
                 tile.classList.add("horizontal-line");
             }
