d7363bd1509c9dfd87dedb77b524d7d856963577
Devdipoo <emiolaajileye@gmail.com>
2024-06-14 09:43:11 -0700

    Hint System

--- src/main.js
+++ src/main.js
@@ -50,9 +50,9 @@ function setGame() {
 
     // Board 9x9
     for (let r = 0; r < 9; r++) {
-        // for (let c = 0; c < 9; c++) {
-            let tile = document.createElement("div");
-            tile.id = r.toString() + "-" + c.toString();
+        for (let c = 0; c < 9; c++) {
+            // let tile = document.createElement("div");
+            // tile.id = r.toString() + "-" + c.toString();
             if (board[r][c] != "-") {
                 tile.innerText = board[r][c];
                 tile.classList.add("tile-start");
